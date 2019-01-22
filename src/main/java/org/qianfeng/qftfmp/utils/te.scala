9f5b23a2fb8d8a82daeca101159df005066ee6cd
Iridescent-life <yuxiabuyouren@126.com>
2019-01-22 09:05:59 +0800

    commit all file1

---
+++ src/main/java/org/qianfeng/qftfmp/utils/te.scala
@@ -0,0 +1,13 @@
+package org.qianfeng.qftfmp.utils
+
+/**
+  * @Auther: xinyu
+  * @Date: 2019/1/22 09:02
+  * @Description:
+  */
+object te {
+  def main(args: Array[String]): Unit = {
+    val a = 0;
+  }
+
+}
